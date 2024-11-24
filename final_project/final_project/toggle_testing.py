bfa033aa8d9e9ad5d6c406a16507672cbad9c920
emilyngu8 <nguyen.emily@northeastern.edu>
2024-11-24 06:56:12 -0500

    sorting code and re-organizing

--- final_project/final_project/toggle_testing.py
+++ final_project/final_project/toggle_testing.py
@@ -1,3 +1,25 @@
+"""
+def get_theme():
+
+    #Determines the current theme of the application (light or dark).
+    #Returns:
+    #    str: 'dark' if dark mode is active, otherwise 'default'.
+
+    args = pn.state.session_args
+    if "theme" in args and args["theme"][0] == b"dark":
+        return "dark"
+    return "default"
+
+current_theme = get_theme()
+
+background_color='#ffffff' if get_theme() == 'default' else '#181818'
+
+theme=current_theme,  # Dynamically set the theme
+    theme_toggle=True,  # Enable toggle for switching themes
+"""
+
+
+
 import panel as pn
 
 pn.extension()
--- final_project/final_project/world_map_testing.py
+++ final_project/final_project/world_map_testing.py
@@ -1,24 +1,3 @@
-"""
-def get_theme():
-
-    #Determines the current theme of the application (light or dark).
-    #Returns:
-    #    str: 'dark' if dark mode is active, otherwise 'default'.
-
-    args = pn.state.session_args
-    if "theme" in args and args["theme"][0] == b"dark":
-        return "dark"
-    return "default"
-
-current_theme = get_theme()
-
-background_color='#ffffff' if get_theme() == 'default' else '#181818'
-
-theme=current_theme,  # Dynamically set the theme
-    theme_toggle=True,  # Enable toggle for switching themes
-"""
-
-
 # Testing world_map
 import geoviews as gv
 from geoviews import opts
