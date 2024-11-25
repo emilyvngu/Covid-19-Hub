0177af0f8fce8a10e5ca5f81573966d179962aa4
Riddhi <riddhi400@gmail.com>
2024-11-25 10:04:10 -0500

    added hive support

--- session.py
+++ session.py
@@ -1,5 +1,23 @@
 from pyspark.sql import SparkSession
 
 def create_session():
-    spark = SparkSession.builder.appName('Practice').getOrCreate()
+    spark = SparkSession.builder.appName('Athena').getOrCreate()
     return spark
+
+from pyspark.sql import SparkSession
+
+def create_session_hive():
+    # Initialize Spark session with Hive support
+    spark = (
+        SparkSession.builder
+        .appName('Athena')
+        .config('spark.sql.catalogImplementation', 'hive')
+        .config('spark.sql.hive.metastore.uris', 'thrift://localhost:9083')  # Metastore URI
+        .config('spark.sql.warehouse.dir', '/user/hive/warehouse')  # HDFS or local path
+        .enableHiveSupport()
+        .getOrCreate()
+    )
+    return spark
+
+
+
