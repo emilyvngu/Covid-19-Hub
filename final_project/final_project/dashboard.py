a882034eb2ed95bb127355cd9f43f08bd400f4c6
emilyngu8 <nguyen.emily@northeastern.edu>
2024-11-30 19:42:59 -0500

    changed variable names, start integrating real values (in comments)

--- final_project/final_project/dashboard.py
+++ final_project/final_project/dashboard.py
@@ -16,16 +16,22 @@ gv.extension('bokeh')
 # Data:
 
 # global data
-global_data = {'total_cases':500, 'total_deaths':100, 'total_recovered':200, 'total_active':200}
+global_json = {'total_cases':500, 'total_deaths':100, 'total_recovered':200, 'total_active':200}
+# global_json = get_total_cases()
 
+#____________________________________________________________________________________
 # country-specific data
-countries = ["USA", "India", "Brazil", "Russia", "France", "Japan"]
-country_total_case_data = {'US':{'total_cases':900, 'total_deaths':100, 'total_recovered':200, 'total_active':200},
+# country_totals_json = get_total_cases_by_country()
+# (not needed) countries = ["USA", "India", "Brazil", "Russia", "France", "Japan"]
+country_totals_json = {'US':{'total_cases':900, 'total_deaths':100, 'total_recovered':200, 'total_active':200},
             'India':{'total_cases':800, 'total_deaths':100, 'total_recovered':200, 'total_active':200},
             'Italy':{'total_cases':700, 'total_deaths':100, 'total_recovered':200, 'total_active':200}}
-df = pd.DataFrame(country_total_case_data)
+country_totals_df = pd.DataFrame(country_totals_json)
 
-world_map_data = [
+#____________________________________________________________________________________
+# Data with each country's location and case-death ratio
+# world_map_json = get_case_fatality_ratio()
+world_map_json = [
     {'country': 'USA', 'lon': -95.7129, 'lat': 37.0902, 'case_death_ratio': 0.05},
     {'country': 'India', 'lon': 78.9629, 'lat': 20.5937, 'case_death_ratio': 0.03},
     {'country': 'Brazil', 'lon': -51.9253, 'lat': -14.2350, 'case_death_ratio': 0.06},
@@ -33,9 +39,22 @@ world_map_data = [
     {'country': 'France', 'lon': 2.2137, 'lat': 46.6034, 'case_death_ratio': 0.04},
     {'country': 'Australia', 'lon': 133.7751, 'lat': -25.2744, 'case_death_ratio': 0.01}
 ]
-world_map_df = pd.DataFrame(world_map_data)
+world_map_df = pd.DataFrame(world_map_json)
 world_map_df_sorted = world_map_df.sort_values(by='case_death_ratio', ascending=False)
 
+#____________________________________________________________________________________
+# Data of total cases, deaths, and recovered over time:
+# trend_json = get_total_cases_over_time()
+# trends_df = pd.DataFrame(trend_json)
+
+time = pd.date_range(start='2020-01-01', periods=50, freq='M')
+cases = np.random.randint(100000, 1000000, 50)
+deaths = np.random.randint(1000, 10000, 50)
+recovered = np.random.randint(50000, 100000, 50)
+
+trends_df = pd.DataFrame({'Date': time, 'Cases': cases, 'Deaths': deaths, 'Recovered': recovered})
+
+#____________________________________________________________________________________
 country_data = pd.DataFrame({
     "USA": [900, 100, 200, 200],
     "India": [800, 100, 200, 200],
@@ -43,13 +62,14 @@ country_data = pd.DataFrame({
     "Russia": [600, 90, 210, 190],
 }, index=["Total Cases", "Total Deaths", "Total Recovered", "Total Active"]).T
 
+#____________________________________________________________________________________
 # Countries with its corresponding case_death_ratio - Needed for country_selector widget:
-options = {row['country']: f"{row['case_death_ratio']:.2%}" for _, row in world_map_df_sorted.iterrows()}
+options = {row['country']: f"{row['case_death_ratio']:.2%}" for _, row in world_map_df.iterrows()}
 
 # Widgets:
 
 # For selecting a specific country:
-country_selector = pn.widgets.Select(name='Select Country', options=df.columns.tolist(), width=280)
+country_selector = pn.widgets.Select(name='Select Country', options=country_totals_df.columns.tolist(), width=280)
 # Search bar widget to search for news - NEWS API
 search_bar = pn.widgets.TextInput(
     name="Search News",
@@ -64,10 +84,10 @@ def other_global_stats():
     """
     stats_md = f"""
     ### Global Statistics
-    - **Total Cases:** {global_data['total_cases']:,}
-    - **Total Deaths:** {global_data['total_deaths']:,}
-    - **Total Recovered:** {global_data['total_recovered']:,}
-    - **Total Active:** {global_data['total_active']:,}
+    - **Total Cases:** {global_json['total_cases']:,}
+    - **Total Deaths:** {global_json['total_deaths']:,}
+    - **Total Recovered:** {global_json['total_recovered']:,}
+    - **Total Active:** {global_json['total_active']:,}
     """
     return pn.pane.Markdown(stats_md, width=300, style={"padding": "10px", "font-size": "14px"})
 
@@ -80,7 +100,7 @@ def country_stats(country):
     if not country:
         return pn.pane.Markdown("### Select a country to view its statistics.", width=300)
 
-    country_data = df[country]
+    country_data = country_totals_df[country]
     stats_md = f"""
     ### {country} Statistics
     - **Total Cases:** {country_data['total_cases']:,}
@@ -96,7 +116,7 @@ def create_global_pie_chart():
     """
     global_df = pd.DataFrame({
         'Category': ['Total Cases', 'Total Deaths', 'Total Recovered', 'Total Active'],
-        'Count': list(global_data.values())
+        'Count': list(global_json.values())
     })
     fig = px.pie(
         global_df,
@@ -119,9 +139,7 @@ def create_country_pie_chart(country):
     """ Generates the pie chart using selected country's statistics
         Country is selected through country_selector widget
     """
-    if country not in country_total_case_data:
-        return pn.pane.Markdown("### Select a valid country")
-    country_data = country_total_case_data[country]
+    country_data = country_totals_json[country]
     country_df = pd.DataFrame({
         'Category': ['Total Cases', 'Total Deaths', 'Total Recovered', 'Total Active'],
         'Count': list(country_data.values())
@@ -145,13 +163,7 @@ def create_country_pie_chart(country):
 def create_line_chart():
     """ Generates line chart of total cases, deaths, and recovered over time
     """
-    time = pd.date_range(start='2020-01-01', periods=50, freq='M')
-    cases = np.random.randint(100000, 1000000, 50)
-    deaths = np.random.randint(1000, 10000, 50)
-    recovered = np.random.randint(50000, 100000, 50)
-
-    df_trends = pd.DataFrame({'Date': time, 'Cases': cases, 'Deaths': deaths, 'Recovered': recovered})
-    fig = px.line(df_trends, x='Date', y=['Cases', 'Deaths', 'Recovered'],
+    fig = px.line(trends_df, x='Date', y=['Cases', 'Deaths', 'Recovered'],
                   labels={'value': 'Count', 'variable': 'Metric'},
                   title='COVID-19 Trends Over Time',
                   height=350)
@@ -295,7 +307,7 @@ layout = pn.template.FastListTemplate(
             ("Case-Fatality Ratio Map", pn.Column( # Tab 2
                 pn.Row(
                     pn.Column(generate_case_fatality_map(), sizing_mode="stretch_width"),  # World map
-                            create_ranking_box(world_map_df_sorted, height=441, width=210), # Ranking of ratio on right
+                            create_ranking_box(world_map_df, height=441, width=210), # Ranking of ratio on right
                 )
             ))
         )
